24280c32312289b40df62384cfb0f03e50784647
Dkaaijk <302345447@student.rocmondriaan.nl>
2024-06-17 12:23:57 +0200

    added chance file to the main

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -7,3 +7,7 @@ func _ready():
 	score_manager = get_node("res://scenes/ScoreManager.gd")
 	MusicController.play_music() 
 
+
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://scenes/lvl1.tscn")
